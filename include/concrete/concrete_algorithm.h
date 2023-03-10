0106b49c3e5e139698aef55a819d3f7aedb5d564
Tszhim <tszhim0320@gmail.com>
2023-03-10 17:50:18 -0500

    upload buggy algorithm code

--- include/concrete/concrete_algorithm.h
+++ include/concrete/concrete_algorithm.h
@@ -3,8 +3,13 @@
 
 #include <unordered_map>
 #include <unordered_set>
-#include <stack>
+#include <vector>
+#include <queue>
+#include <deque>
 #include "abstract_algorithm.h"
+#include "concrete_battery_meter.h"
+#include "concrete_dirt_sensor.h"
+#include "concrete_walls_sensor.h"
 #include "coordinate.h"
 #include "hash.h"
 
@@ -19,7 +24,7 @@ public:
     /**
      * @brief Constructs a "ConcreteAlgorithm" object.
      */
-    ConcreteAlgorithm() {}
+    ConcreteAlgorithm() : stepCount(0), branchLevel(0), nextSpaceBacktrack(false), nextSpacesSet(false), mapFinished(false) {}
 
     /**
      * @brief Destroys a "ConcreteAlgorithm" object.
@@ -57,15 +62,69 @@ public:
     Step nextStep();
 
 private:
-    size_t missionBudget;  // The number of steps allocated to the robot for the mission.
+    /* Set before algorithm runs. */
+    size_t missionBudget;  
     const BatteryMeter* bm;
     const DirtSensor* ds;
     const WallsSensor* ws;
 
-    std::unordered_map<Coordinate, int, cHash> nodes;        // Algorithm view of all valid spaces in the house and their dirt levels.
-    std::unordered_set<Coordinate, Coordinate, cHash> edges; // Algorithm view of all connections between valid spaces in the house.
-    std::stack<Coordinate> branch_stack;                     // Maintains the list of branches yet to be traversed.
-    std::stack<Coordinate> path_stack;                       // Maintains the current path the robot is on from charging dock.
+    /* Holds sensor data when pulled. */
+    int batteryLeft;
+    int dirt;
+    bool wallNorth, wallWest, wallSouth, wallEast;
+    
+    /* Maintained by algorithm. */
+    int batteryCap;
+    int stepCount;                                               // Maintains number of steps taken.
+    Coordinate space;                                            // Maintains current robot position.
+    std::queue<Coordinate> nextSpaces;                           // Maintains the cluster of coordinates the robot should move to next for a particular branch.
+    bool nextSpaceBacktrack;                                     // For each cluster of coordinates, the robot should backtrack once after visiting each one.
+    bool nextSpacesSet;                                          // Whether the next set of spaces for branching is determined.
+    bool mapFinished;                                            // Whether the house mapping is completed.
+    int branchLevel;                                             // BFS level.
+
+    std::unordered_map<Coordinate, int, cHash> nodes;                     // Algorithm view of all valid spaces in the house and their dirt levels.
+    std::unordered_set<Coordinate, cHash> visitedNodes;                   // Maintains the list of nodes already visited.
+    std::unordered_map<Coordinate, std::deque<Coordinate>, cHash> paths;  // Maintains the path to reach a particular node.
+    std::queue<std::queue<Coordinate>> branchQueue;                       // Maintains the list of branches yet to be traversed for the current level.
+    std::queue<std::queue<Coordinate>> nextQueue;                         // Maintains the list of branches yet to be traversed for the next level.
+    std::deque<Coordinate> pathStack;                                     // Maintains the current path the robot is on from charging dock.
+    
+    /**
+     * @brief Prepares class attributes before next algorithm iteration.
+    */
+    void setup();
+
+    /**
+     * @brief Makes the next step in order to get from the current coordinate to the next.
+     * @param next The next coordinate.
+     * @return The next step made.
+    */
+    Step makeStepToNext(Coordinate next);
+    /**
+     * @brief Move back to charging dock using path stack.
+     * @return The next step to make to backtrack.
+     */
+    Step backtrack();
+
+    /**
+     * @brief Move to the next space determined by BFS traversal.
+     * @return The next step to make to reach next space.
+     */
+    Step moveTo();
+
+     /**
+     * @brief Continue phase #1 of the algorithm: mapping the entire house. 
+     * @return The next step to make to map the house. 
+     */
+    Step phase1();
+
+    /**
+     * @brief Continue phase #2 of the algorithm: cleaning the mapped house.
+     * @return The next step to make to clean the house.
+     */
+    Step phase2();
+    
 };
 
 #endif
--- src/concrete/concrete_algorithm.cpp
+++ src/concrete/concrete_algorithm.cpp
@@ -2,22 +2,187 @@
 
 void ConcreteAlgorithm::setMaxSteps(const std::size_t maxSteps) {
     this->missionBudget = maxSteps;
+    this->stepCount = 0;
 }
 
-void ConcreteAlgorithm::setBatteryMeter(const BatteryMeter& batteryMeter) {
+void ConcreteAlgorithm::setBatteryMeter(const BatteryMeter &batteryMeter) {
     this->bm = &batteryMeter;
 }
 
-void ConcreteAlgorithm::setDirtSensor(const DirtSensor& dirtSensor) {
+void ConcreteAlgorithm::setDirtSensor(const DirtSensor &dirtSensor) {
     this->ds = &dirtSensor;
 }
 
-void ConcreteAlgorithm::setWallsSensor(const WallsSensor& wallsSensor) {
+void ConcreteAlgorithm::setWallsSensor(const WallsSensor &wallsSensor) {
     this->ws = &wallsSensor;
 }
 
 Step ConcreteAlgorithm::nextStep() {
-    //TODO:
-    return Step::North;
+    setup();
+
+    /* Return finish when no more dirt cleanable with remaining mission budget and robot returned to charging dock. */
+    if (this->stepCount >= this->missionBudget && this->space.x == 0 && this->space.y == 0)
+        return Step::Finish;
+
+    /* At this point, a move will be made. */
+    this->stepCount++;
+
+    /* If on charging dock, charge until full capacity. */
+    if(this->space.x == 0 && this->space.y == 0 && this->batteryLeft < this->batteryCap) 
+        return Step::Stay;
+
+    /* Robot cannot continue traversal with remaining mission budget. */
+    if (this->missionBudget <= this->stepCount + this->pathStack.size() + 1)
+        return backtrack();
+
+    /* Robot cannot continue traversal with remaining battery, must return to dock. */
+    if (this->batteryLeft <= this->pathStack.size() + 1)
+        return backtrack();
+
+    /* If phase #1 is finished, return to dock, then continue with phase #2. */
+    if (this->pathStack.empty() && (this->branchQueue.empty() || this->branchLevel > this->batteryCap / 2))
+        this->mapFinished = true;
+
+    return !this->mapFinished ? phase1() : phase2();
 }
 
+void ConcreteAlgorithm::setup() {
+    /* Pull data from sensors. */   
+    this->batteryLeft = dynamic_cast<const ConcreteBatteryMeter *>(this->bm)->getBatteryState();
+    this->dirt = dynamic_cast<const ConcreteDirtSensor *>(this->ds)->dirtLevel();
+    this->wallNorth = dynamic_cast<const ConcreteWallsSensor *>(this->ws)->isWall(Direction::North);
+    this->wallWest = dynamic_cast<const ConcreteWallsSensor *>(this->ws)->isWall(Direction::West);
+    this->wallSouth = dynamic_cast<const ConcreteWallsSensor *>(this->ws)->isWall(Direction::South);
+    this->wallEast = dynamic_cast<const ConcreteWallsSensor *>(this->ws)->isWall(Direction::East);
+
+    /* Initialize some class attributes on first run. */
+    if (stepCount == 0) {
+        this->batteryCap = this->batteryLeft;
+        this->branchQueue.push(std::queue<Coordinate>({Coordinate()}));
+    }
+}
+
+Step ConcreteAlgorithm::makeStepToNext(Coordinate next) {
+    if(next.y > this->space.y) {
+        this->space.y++;
+        return Step::North;
+    }
+    if(next.x < this->space.x) {
+        this->space.x--;
+        return Step::West;
+    }
+    if(next.y < this->space.y) {
+        this->space.y--;
+        return Step::South;
+    }
+    if(next.x > this->space.x) {
+        this->space.x++;
+        return Step::East;
+    }
+
+    /* Unexpected. */
+    return Step::Stay;
+}
+
+Step ConcreteAlgorithm::backtrack() {
+    /* Unexpected error. */
+    if (this->pathStack.empty())
+        return Step::Stay;
+    
+    Coordinate prev = this->pathStack.back();
+    this->pathStack.pop_back();
+
+    return makeStepToNext(prev);
+}
+
+Step ConcreteAlgorithm::moveTo() {
+    /* Get back to the cluster. */
+    if(auto it = paths.find(this->nextSpaces.front()); it != paths.end()) {
+        auto path = it->second;
+        while(path.front() != space)
+            path.pop_front();
+        
+        if(!path.empty()) {
+            Coordinate front = path.front();
+            path.pop_front();
+            return makeStepToNext(front);
+        }
+    }
+
+    /* Backtrack one step after each visit to prepare for next move in cluster. */
+    if(this->nextSpaceBacktrack) {
+        this->nextSpaceBacktrack = false;
+        return backtrack();
+    }
+
+    /* Move to next node in cluster. */
+    Coordinate nextSpace = this->nextSpaces.front();
+    this->nextSpaces.pop();
+
+    Step s = makeStepToNext(nextSpace);
+    this->nextSpaceBacktrack = true;
+
+    /* Delete next space in cluster. */
+    if(this->nextSpaces.empty()) {
+        this->nextSpacesSet = false;
+    }
+    return s;
+}
+
+Step ConcreteAlgorithm::phase1() {
+    /* Return back to dock once a cluster has been visited. */
+    if(!this->nextSpacesSet && (this->space.x != 0 || this->space.y != 0))
+        return backtrack();
+
+    /* Housekeeping. */
+    this->pathStack.push_back(this->space);
+    this->nodes.insert({this->space, this->dirt}); 
+    this->visitedNodes.insert(this->space);
+
+    /* Pop out next cluster of nodes to traverse to if not determined yet.  */
+    if(!this->nextSpacesSet) {
+        this->nextSpaces = this->branchQueue.front();
+        this->branchQueue.pop();
+        this->nextSpacesSet = true;
+    }
+
+    /* Push available nodes in the order NWSE. */
+    Coordinate north = Coordinate(this->space.x, this->space.y + 1);
+    Coordinate west = Coordinate(this->space.x - 1, this->space.y);
+    Coordinate south = Coordinate(this->space.x, this->space.y - 1);
+    Coordinate east = Coordinate(this->space.x + 1, this->space.y);
+    std::queue<Coordinate> nextBranch;
+
+    if(this->wallNorth && !this->visitedNodes.count(north)) {
+        nextBranch.push(north);
+        this->paths.insert({north, pathStack});
+    }
+    if(this->wallWest && !this->visitedNodes.count(west)) {
+        nextBranch.push(west);
+        this->paths.insert({west, pathStack});
+    }
+    if(this->wallSouth && !this->visitedNodes.count(south)) {
+        nextBranch.push(south);
+        this->paths.insert({south, pathStack});
+    }
+    if(this->wallEast && !this->visitedNodes.count(east)) {
+        nextBranch.push(east);
+        this->paths.insert({east, pathStack});
+    }
+    if(!this->visitedNodes.count(this->space) && !nextBranch.empty())
+        this->nextQueue.push(nextBranch);
+        
+    /* Reached next BFS depth level. */
+    if(this->branchQueue.empty()) {
+        this->branchQueue = this->nextQueue;
+        this->nextQueue = std::queue<std::queue<Coordinate>>();
+        this->branchLevel++;
+    }
+
+    /* Move to next node. */    
+    return moveTo();
+}
+
+Step ConcreteAlgorithm::phase2() {
+    return Step::Finish;
+}
